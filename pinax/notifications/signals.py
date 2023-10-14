97591fe212e16e42849ab50ddc02383b2a3ba2e0
powderflask <powderflask@gmail.com>
2023-10-14 15:01:25 -0700

    fix dj4.2 breaking change

--- pinax/notifications/signals.py
+++ pinax/notifications/signals.py
@@ -1,5 +1,5 @@
 import django.dispatch
 
-emitted_notices = django.dispatch.Signal(
-    providing_args=["batches", "sent", "sent_actual", "run_time"]
-)
+# providing_args=["batches", "sent", "sent_actual", "run_time"]  # Removed in dj4.x
+emitted_notices = django.dispatch.Signal()
+
