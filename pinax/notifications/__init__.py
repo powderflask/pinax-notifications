782255357b773dbc09e5151b78adf7229d5d6477
powderflask <powderflask@gmail.com>
2025-11-25 11:53:49 -0800

    replace deprecated reference to pkg_resources - back-compatible to continue support for old Python versions

--- pinax/notifications/__init__.py
+++ pinax/notifications/__init__.py
@@ -1,6 +1,15 @@
-import pkg_resources
+try:
+    from importlib.metadata import version
+except ImportError:
+    # Python < 3.8 fallback
+    from pkg_resources import get_distribution
+
+    def version(package_name):
+        return get_distribution(package_name).version
+
 import django
 
 if django.VERSION < (3, 2):
     default_app_config = "pinax.notifications.apps.AppConfig"
-__version__ = pkg_resources.get_distribution("pinax-notifications").version
+
+__version__ = version("pinax-notifications")
